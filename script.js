5d0915db8f11a6f0e37b8591961afbf42bd4c4f6
camillaevani <camillaevani21@gmail.com>
2025-05-12 10:45:45 +0000

    added dropdown nav and responsive

---
+++ script.js
@@ -0,0 +1,18 @@
+function onPageLoad() {
+    //this is a constant
+    const btn = document.querySelector ('.nav-button')
+    const nav = document.querySelector ('.nav-navigation')
+    //ad a listener on btn for click
+    btn.addEventListener('click', function() {
+        if (nav.classList.contains('open')) {
+            //if the menu is open
+            nav.classList.remove('open')
+        }
+        else {
+            //if the menu is closed
+            nav.classList.add('open')
+        }
+    })
+}
+
+window.addEventListener('load', onPageLoad)
